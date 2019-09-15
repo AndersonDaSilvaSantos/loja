fdd381f1dee063587496d84a980c443ad0d30282
AndersonDaSilvaSantos <anderson_silva05@hotmail.com>
2019-09-15 15:24:41 -0300

    Atualizando

--- websystem/src/main/java/com/system/websystem/repository/FornecedorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.system.websystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.system.websystem.models.Fornecedor;
-
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
-
-}
--- websystem/src/main/java/com/system/websystem/repository/VendaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.system.websystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.system.websystem.models.Venda;
-
-public interface VendaRepository extends JpaRepository<Venda, Long> {
-
-}
